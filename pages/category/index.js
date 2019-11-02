9060960acfebe7780a56f8f59c935949519c6a39
WJFeiOne <ww2233597457@163.com>
2019-11-02 15:07:02 +0800

    05-实现分类页面功能

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,37 @@
-// pages/category/index.js
+/* 分类页面 js */
+
+
+// 导入 路由配置 模块
+import request from "../../utils/request.js"
+
+// 分类页面 实例
 Page({
-
-    /**
-     * 页面的初始数据
-     */
+    // 指定 分类页面 数据
     data: {
-
+        current: 0,  // 接收 当前选中 索引
+        list: []     // 接收 分类列表 数据
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
+ 
+    // 页面加载 完毕执行
     onLoad: function (options) {
-
+        // 请求 分类页面 数据
+        request({
+            url: "/api/public/v1/categories"   // 请求 数据接口
+        }).then(res => {
+            const { message } = res.data;      // 解构 响应数据
+            this.setData({
+                list: message                  // 保存 分类数据
+            })
+        })
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    // 点击 左侧的菜单 时候触发
+    handleChange(event) {
+        // 获取 当前选中 索引值
+        const { index } = event.target.dataset;
+        
+        this.setData({
+            current: index   // 设置 当前选中 索引值
+        })
     }
 })
