34f09b18f02979678f6cd7c00eee49a50c11456f
WJFeiOne <ww2233597457@163.com>
2019-11-03 16:24:11 +0800

    09-优化商品列表搜索功能

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -11,18 +11,100 @@ Page({
         goods: [],      // 接收 商品列表 数据
         hasMore: true,  // 接收 加载更多 状态
         pagenum: 1,     // 接收 当前商品 页数
-        loading: false  // 接收 函数节流 数据
+        loading: false,      // 接收 函数节流 数据
+        showCancel: false,   // 接收 显示取消按钮 参数
+        searchValue: "",     // 接收 输入框的 数据
     },
 
     // 页面加载 完毕执行
     onLoad: function (options) {
         // 从 options 解构搜索关键词参数
-        const { query } = options;
+        const { searchValue } = options;
         
-        // 设置 关键词参数
+        // 设置 输入框 参数
         this.setData({
-            query
+            searchValue
         });
+
+        // 设置 关键词 参数
+        this.setData({
+            query:searchValue
+        });
+
+        // 请求 列表数据
+        this.getList();
+    },
+
+
+    // 监听 输入框输入 时候的事件
+    handleInput(event) {
+        const { value } = event.detail;  // 获取 输入框的 数据
+        let showCancel;                  // 定义 取消按钮 参数
+
+        // 判断输入框有没值, value.trim()去除前后空格
+        showCancel = value ? true : false
+
+        // 设置 搜索参数
+        this.setData({
+            showCancel,         // 保存 取消按钮 状态
+            searchValue: value  // 保存 输入框的 数据
+        })
+    },
+
+    // 点击 取消按钮时候触发
+    handleCancel() {
+        // 设置 输入框参数
+        this.setData({
+            showCancel: false,  // 隐藏 取消按钮 
+            searchValue: ""     // 清空 输入框 数据
+        })
+    },
+
+    // 点击 键盘确定按钮时候触发
+    handlleConfirm() {
+        // 从本地存储 获取搜索历史 数据
+        const arr = wx.getStorageSync('search') || [];
+
+        // 判断 输入框的值是否为空
+        if (!this.data.searchValue) {
+            return;     // 退出
+        }
+
+        let lisi = [];  // 定义 搜索历史数组
+
+        const iten = this.data.searchValue    // 获取 输入框的值
+
+        // 判断 搜索历史不为空
+        if (arr.length != 0) {
+            // 遍历 搜索历史
+            arr.forEach(v => {
+                // 判断 是否已有搜索历史
+                if (v != iten) {
+                    lisi.push(v);   // 去除 相同的搜索历史
+                }
+            })
+            lisi.unshift(iten);     // 添加 搜索历史数据
+        } else {
+            lisi.unshift(iten);     // 添加 搜索历史数据
+        }
+
+        // 将搜索参数 保存到本地
+        wx.setStorageSync('search', lisi.slice(0, 10));
+
+        // 设置 关键词 参数
+        this.setData({
+            query: iten
+        });
+
+        // 设置 加载更多 参数
+        this.setData({
+            pagenum: 1 // 不显示 加载更多
+        })
+
+        // 设置 加载更多 参数
+        this.setData({
+            goods:[] // 不显示 加载更多
+        })
 
         // 请求 列表数据
         this.getList();
